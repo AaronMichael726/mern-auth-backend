1f75d1bae48266d37c9b0a93fc91f27359dbe8f4
aaronmichael726 <aaronmichael726@gmail.com>
2021-03-04 15:53:48 -0600

    register

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const { user } = require('../controllers');
 const ctrl = require('../controllers');
 
+router.get('/test', ctrl.user.test)
+router.post('/register', ctrl.user.register)
+
+module.exports = user
